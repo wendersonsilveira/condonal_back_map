86067120841804922b7c74fcd5655e92ab659431
Wenderson Silveira <wenderson@gestartcondominios.com.br>
2022-03-07 13:41:05 -0300

    teste 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const csvFilePath = './condominios.csv'
 const csv = require('csvtojson')
 const bodyParser = require('body-parser')
